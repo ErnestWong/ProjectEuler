6bf5262e20bdb13528e3b48ba66086e12ae2bad6
ErnestWong <ewong2222@hotmail.com>
2014-06-05 14:28:50 -0400

    Non-abundant sums
    A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
    
    A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
    
    As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
    
    Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.

--- projecteuler23.java
+++ projecteuler23.java
@@ -1,7 +1,11 @@
 import java.util.*;
 
+//Answer: 4179871
 public class Problem23 {
-
+	
+	/**
+	 *method determines if number is abundant (sum of divisors greater than number)
+	**/
 	public static boolean isAbundant(int n) {
 		int divisorsum = 0;
 
@@ -16,6 +20,9 @@ public class Problem23 {
 			return false;
 	}
 
+	/**
+	 * returns list of sorted abundant numbers (upper bound is 28123)
+	**/
 	public static List<Integer> findAbundants() {
 		List<Integer> abundant = new ArrayList<Integer>();
 
@@ -27,16 +34,26 @@ public class Problem23 {
 		Collections.sort(abundant);
 		return abundant;
 	}
-
+	
 	public static void doproblem() {
 		List<Integer> sortedAbundant = findAbundants();
+		
+		//duplicate list to hashset for faster search
 		Set<Integer> sortedSet = new HashSet<Integer>(sortedAbundant);
 		int sum = 0;	
+		
+		//loop through all the numbers up to 28123 as possible numbers that cannot be
+		//made from sum of abundant numbers
 		for (int i = 0; i < 28123; i++) {
 			boolean noSum = true;
+			
+			//loop through abundant numbers; if a+b = i, where a+b are abundant numbers,
+			//then b = i-a should be found in the hashset. If it is not found, then false
+			//Using this method with hashset reduces complexity to O(n)
 			for(int a = 0; a < sortedAbundant.size(); a++){
 				int diff = i - sortedAbundant.get(a);
-				if(sortedSet.contains(diff)) noSum = false;
+				if(sortedSet.contains(diff)) 
+					noSum = false;
 			}
 
 			if (noSum){
